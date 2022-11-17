0adaed5228280ef6e71309f46ef26b2c06468956
Adrien Foulon <6115458+Tofandel@users.noreply.github.com>
2022-11-17 23:04:41 +0100

    Add french translation

---
+++ src/locale/fr-FR.ts
@@ -0,0 +1,89 @@
+import type { StaticTextDefaultValue } from '../../index';
+
+/**
+ * @author Adrien Foulon
+ * @github https://github.com/tofandel
+ * Author is native French
+ */
+const FR_FR: StaticTextDefaultValue = {
+  toolbarTips: {
+    bold: 'Gras',
+    underline: 'Souligné',
+    italic: 'Italic',
+    strikeThrough: 'Barré',
+    title: 'Titre',
+    sub: 'Indice',
+    sup: 'Exposant',
+    quote: 'Citation',
+    unorderedList: 'Liste',
+    orderedList: 'Liste numérique',
+    task: 'Liste de tâche',
+    codeRow: 'Code intégré',
+    code: 'Bloc de code',
+    link: 'Lien',
+    image: 'Image',
+    table: 'Table',
+    mermaid: 'Organigramme',
+    katex: 'Formule',
+    revoke: 'Annuler',
+    next: 'Refaire',
+    save: 'Sauvegarder',
+    prettier: 'Sublimer',
+    pageFullscreen: 'Passer en pleine page',
+    fullscreen: 'Passer en plein écran',
+    preview: 'Prévisualiser',
+    htmlPreview: 'Prévisualiser le HTML',
+    catalog: 'Table des matières',
+    github: 'Code source de l'editeur',
+  },
+  titleItem: {
+    h1: 'Titre niveau 1',
+    h2: 'Titre niveau 2',
+    h3: 'Titre niveau 3',
+    h4: 'Titre niveau 4',
+    h5: 'Titre niveau 5',
+    h6: 'Titre niveau 6',
+  },
+  imgTitleItem: {
+    link: 'Ajouter depuis une url',
+    upload: 'Télécharger une image',
+    clip2upload: 'Télécharger et recadrer',
+  },
+  linkModalTips: {
+    title: 'Ajouter un lien',
+    descLable: 'Texte ：',
+    descLablePlaceHolder: 'Entrez un texte...',
+    urlLable: 'Url ：',
+    urlLablePlaceHolder: 'Entrez une url...',
+    buttonOK: 'Valider',
+  },
+  clipModalTips: {
+    title: 'Télécharger et recadrer une image',
+    buttonUpload: 'Valider',
+  },
+  copyCode: {
+    text: 'Copier le code',
+    successTips: 'Copié',
+    failTips: 'La copie a échoué！',
+  },
+  mermaid: {
+    flow: 'Organigramme',
+    sequence: 'Chronogramme',
+    gantt: 'Diagramme de Gantt',
+    class: 'Diagramme de classes',
+    state: 'Diagramme d'état',
+    pie: 'Diagramme circulaire',
+    relationship: 'Diagramme de relation',
+    journey: 'Diagramme de tâches',
+  },
+  katex: {
+    inline: 'Formule en ligne',
+    block: 'Formule en bloc',
+  },
+  footer: {
+    markdownTotal: 'Nombre de mots',
+    scrollAuto: 'Défilement synchronisé',
+  },
+};
+
+export default FR_FR;
