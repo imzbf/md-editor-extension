c1378d86631be744fa18934bc077d236ded951ed
Lars Marondel <lars.marondel@lawa-solutions.com>
2024-05-10 10:26:12 +0200

    Add German translation

---
+++ packages/common/src/locale/de-DE.ts
@@ -0,0 +1,90 @@
+import type { StaticTextDefaultValue } from '../../index';
+
+/**
+ * @author Lars Marondel
+ * @github https://github.com/JehtJanich
+ * Author is native German speaker
+ */
+const DE_DE: StaticTextDefaultValue = {
+  toolbarTips: {
+    bold: 'Fett',
+    underline: 'Unterstrichen',
+    italic: 'Kursiv',
+    strikeThrough: 'Durchgestrichen',
+    title: 'Titel',
+    sub: 'Tiefgestellt',
+    sup: 'Hochgestellt',
+    quote: 'Zitat',
+    unorderedList: 'Ungeordnete Liste',
+    orderedList: 'Geordnete Liste',
+    task: 'Aufgabenliste',
+    codeRow: 'Eingebetteter Code',
+    code: 'Codeblock',
+    link: 'Link',
+    image: 'Bild',
+    table: 'Tabelle',
+    mermaid: 'Flussdiagramm',
+    katex: 'Formel',
+    revoke: 'Rückgängig machen',
+    next: 'Wiederherstellen',
+    save: 'Speichern',
+    prettier: 'Verschönern',
+    pageFullscreen: 'Vollbildmodus',
+    fullscreen: 'Vollbild',
+    preview: 'Vorschau',
+    htmlPreview: 'HTML Vorschau',
+    catalog: 'Inhaltsverzeichnis',
+    github: 'Quellcode des Editors'
+  },
+  titleItem: {
+    h1: 'Überschrift Ebene 1',
+    h2: 'Überschrift Ebene 2',
+    h3: 'Überschrift Ebene 3',
+    h4: 'Überschrift Ebene 4',
+    h5: 'Überschrift Ebene 5',
+    h6: 'Überschrift Ebene 6'
+  },
+  imgTitleItem: {
+    link: 'Bildlink hinzufügen',
+    upload: 'Bild hochladen',
+    clip2upload: 'Bild zuschneiden und hochladen'
+  },
+  linkModalTips: {
+    linkTitle: 'Link hinzufügen',
+    imageTitle: 'Bild von URL hinzufügen',
+    descLabel: 'Text: ',
+    descLabelPlaceHolder: 'Text eingeben...',
+    urlLabel: 'URL: ',
+    urlLabelPlaceHolder: 'URL eingeben...',
+    buttonOK: 'Bestätigen'
+  },
+  clipModalTips: {
+    title: 'Bild zuschneiden und hochladen',
+    buttonUpload: 'Hochladen'
+  },
+  copyCode: {
+    text: 'Code kopieren',
+    successTips: 'Kopiert!',
+    failTips: 'Kopieren fehlgeschlagen!'
+  },
+  mermaid: {
+    flow: 'Flussdiagramm',
+    sequence: 'Sequenzdiagramm',
+    gantt: 'Gantt-Diagramm',
+    class: 'Klassendiagramm',
+    state: 'Zustandsdiagramm',
+    pie: 'Kreisdiagramm',
+    relationship: 'Beziehungsdiagramm',
+    journey: 'Reisediagramm'
+  },
+  katex: {
+    inline: 'Inline-Formel',
+    block: 'Blockformel'
+  },
+  footer: {
+    markdownTotal: 'Wortanzahl',
+    scrollAuto: 'Automatisches Scrollen'
+  }
+};
+
+export default DE_DE;
