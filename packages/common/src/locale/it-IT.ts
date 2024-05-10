b03225f4dd44dd1e6a30a8fe846e04d204ec8136
Lars Marondel <lars.marondel@lawa-solutions.com>
2024-05-10 10:26:25 +0200

    Add Italien translation

---
+++ packages/common/src/locale/it-IT.ts
@@ -0,0 +1,91 @@
+import type { StaticTextDefaultValue } from '../../index';
+
+/**
+ * @author Lars Marondel
+ * @github https://github.com/JehtJanich
+ * Translations supported by GitHub CoPilot
+ */
+
+const IT_IT: StaticTextDefaultValue = {
+  toolbarTips: {
+    bold: 'Grassetto',
+    underline: 'Sottolineato',
+    italic: 'Corsivo',
+    strikeThrough: 'Barrato',
+    title: 'Titolo',
+    sub: 'Indice',
+    sup: 'Esponente',
+    quote: 'Citazione',
+    unorderedList: 'Elenco non ordinato',
+    orderedList: 'Elenco ordinato',
+    task: 'Lista di compiti',
+    codeRow: 'Codice incorporato',
+    code: 'Blocco di codice',
+    link: 'Link',
+    image: 'Immagine',
+    table: 'Tabella',
+    mermaid: 'Diagramma',
+    katex: 'Formula',
+    revoke: 'Annulla',
+    next: 'Ripristina',
+    save: 'Salva',
+    prettier: 'Formatta',
+    pageFullscreen: 'Pagina a schermo intero',
+    fullscreen: 'Schermo intero',
+    preview: 'Anteprima',
+    htmlPreview: 'Anteprima HTML',
+    catalog: 'Indice',
+    github: 'Codice sorgente dell\'editor'
+  },
+  titleItem: {
+    h1: 'Intestazione Livello 1',
+    h2: 'Intestazione Livello 2',
+    h3: 'Intestazione Livello 3',
+    h4: 'Intestazione Livello 4',
+    h5: 'Intestazione Livello 5',
+    h6: 'Intestazione Livello 6'
+  },
+  imgTitleItem: {
+    link: 'Aggiungi link immagine',
+    upload: 'Carica immagine',
+    clip2upload: 'Ritaglia e carica immagine'
+  },
+  linkModalTips: {
+    linkTitle: 'Aggiungi link',
+    imageTitle: 'Aggiungi immagine da URL',
+    descLabel: 'Testo: ',
+    descLabelPlaceHolder: 'Inserisci il testo...',
+    urlLabel: 'URL: ',
+    urlLabelPlaceHolder: 'Inserisci l\'URL...',
+    buttonOK: 'Conferma'
+  },
+  clipModalTips: {
+    title: 'Ritaglia e carica immagine',
+    buttonUpload: 'Carica'
+  },
+  copyCode: {
+    text: 'Copia codice',
+    successTips: 'Copiato!',
+    failTips: 'Copia fallita!'
+  },
+  mermaid: {
+    flow: 'Diagramma di flusso',
+    sequence: 'Diagramma di sequenza',
+    gantt: 'Diagramma di Gantt',
+    class: 'Diagramma di classe',
+    state: 'Diagramma di stato',
+    pie: 'Diagramma a torta',
+    relationship: 'Diagramma di relazione',
+    journey: 'Diagramma di viaggio'
+  },
+  katex: {
+    inline: 'Formula in linea',
+    block: 'Formula a blocco'
+  },
+  footer: {
+    markdownTotal: 'Conteggio parole',
+    scrollAuto: 'Scorrimento automatico'
+  }
+};
+
+export default IT_IT;
