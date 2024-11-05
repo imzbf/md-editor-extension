97c65b75682a8705794e65342c435f8fdb0668ff
Piotr Perzyna <piotr.perzyna@gmail.com>
2024-11-05 22:35:54 +0100

    Create pl-PL.ts

---
+++ packages/common/src/locale/pl-PL.ts
@@ -0,0 +1,90 @@
+import type { StaticTextDefaultValue } from '../../index';
+
+/**
+ * @author Lars Marondel
+ * @github https://github.com/JehtJanich
+ * Autor jest niemieckojęzyczny
+ */
+const PL_PL: StaticTextDefaultValue = {
+  toolbarTips: {
+    bold: 'Pogrubienie',
+    underline: 'Podkreślenie',
+    italic: 'Kursywa',
+    strikeThrough: 'Przekreślenie',
+    title: 'Tytuł',
+    sub: 'Indeks dolny',
+    sup: 'Indeks górny',
+    quote: 'Cytat',
+    unorderedList: 'Lista nieuporządkowana',
+    orderedList: 'Lista uporządkowana',
+    task: 'Lista zadań',
+    codeRow: 'Wbudowany kod',
+    code: 'Blok kodu',
+    link: 'Link',
+    image: 'Obraz',
+    table: 'Tabela',
+    mermaid: 'Schemat przepływu',
+    katex: 'Formuła',
+    revoke: 'Cofnij',
+    next: 'Przywróć',
+    save: 'Zapisz',
+    prettier: 'Upiększ',
+    pageFullscreen: 'Pełny ekran strony',
+    fullscreen: 'Pełny ekran',
+    preview: 'Podgląd',
+    htmlPreview: 'Podgląd HTML',
+    catalog: 'Spis treści',
+    github: 'Kod źródłowy edytora'
+  },
+  titleItem: {
+    h1: 'Nagłówek poziom 1',
+    h2: 'Nagłówek poziom 2',
+    h3: 'Nagłówek poziom 3',
+    h4: 'Nagłówek poziom 4',
+    h5: 'Nagłówek poziom 5',
+    h6: 'Nagłówek poziom 6'
+  },
+  imgTitleItem: {
+    link: 'Dodaj link do obrazu',
+    upload: 'Prześlij obraz',
+    clip2upload: 'Przytnij i prześlij obraz'
+  },
+  linkModalTips: {
+    linkTitle: 'Dodaj link',
+    imageTitle: 'Dodaj obraz z URL',
+    descLabel: 'Tekst: ',
+    descLabelPlaceHolder: 'Wpisz tekst...',
+    urlLabel: 'URL: ',
+    urlLabelPlaceHolder: 'Wpisz URL...',
+    buttonOK: 'Potwierdź'
+  },
+  clipModalTips: {
+    title: 'Przytnij i prześlij obraz',
+    buttonUpload: 'Prześlij'
+  },
+  copyCode: {
+    text: 'Skopiuj kod',
+    successTips: 'Skopiowano!',
+    failTips: 'Nie udało się skopiować!'
+  },
+  mermaid: {
+    flow: 'Schemat przepływu',
+    sequence: 'Schemat sekwencji',
+    gantt: 'Wykres Gantta',
+    class: 'Diagram klas',
+    state: 'Diagram stanów',
+    pie: 'Diagram kołowy',
+    relationship: 'Diagram relacji',
+    journey: 'Diagram podróży'
+  },
+  katex: {
+    inline: 'Formuła inline',
+    block: 'Formuła blokowa'
+  },
+  footer: {
+    markdownTotal: 'Liczba znaków',
+    scrollAuto: 'Automatyczne przewijanie'
+  }
+};
+
+export default PL_PL;
