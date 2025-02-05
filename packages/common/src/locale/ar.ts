f266af5ff743736f3932cde94637ef1e0e6129b7
Matteo <matteo.sonnenholzner@kiteworks.com>
2025-02-05 16:14:21 +0100

    feat: add Arabic translation file

---
+++ packages/common/src/locale/ar.ts
@@ -0,0 +1,90 @@
+import type { StaticTextDefaultValue } from '../../index';
+
+/**
+ * @author mzner
+ * @github https://github.com/mzner
+ *
+ */
+const AR: StaticTextDefaultValue = {
+  toolbarTips: {
+    bold: 'عريض',
+    underline: 'تحته خط',
+    italic: 'مائل',
+    strikeThrough: 'يتوسطه خط',
+    title: 'عنوان',
+    sub: 'حرف منخفض',
+    sup: 'حرف مرتفع',
+    quote: 'اقتباس',
+    unorderedList: 'قائمة غير مرتبة',
+    orderedList: 'قائمة مرتبة',
+    task: 'قائمة المهام',
+    codeRow: 'كود مضمن',
+    code: 'كود على مستوى الكتلة',
+    link: 'رابط',
+    image: 'صورة',
+    table: 'جدول',
+    mermaid: 'مرميد',
+    katex: 'معادلة',
+    revoke: 'تراجع',
+    next: 'إعادة التراجع',
+    save: 'حفظ',
+    prettier: 'تحسين',
+    pageFullscreen: 'ملء الشاشة داخل الصفحة',
+    fullscreen: 'ملء الشاشة',
+    preview: 'معاينة',
+    previewOnly: 'معاينة فقط',
+    htmlPreview: 'معاينة HTML',
+    catalog: 'فهرس',
+    github: 'كود المصدر'
+  },
+  titleItem: {
+    h1: 'عنوان مستوى 1',
+    h2: 'عنوان مستوى 2',
+    h3: 'عنوان مستوى 3',
+    h4: 'عنوان مستوى 4',
+    h5: 'عنوان مستوى 5',
+    h6: 'عنوان مستوى 6'
+  },
+  imgTitleItem: {
+    link: 'إضافة رابط صورة',
+    upload: 'رفع صورة',
+    clip2upload: 'قص ورفع'
+  },
+  linkModalTips: {
+    linkTitle: 'إضافة',
+    descLabel: 'الوصف:',
+    descLabelPlaceHolder: 'أدخل وصفًا...',
+    urlLabel: 'الرابط:',
+    urlLabelPlaceHolder: 'أدخل رابطًا...',
+    buttonOK: 'موافق'
+  },
+  clipModalTips: {
+    title: 'اقتصاص الصورة',
+    buttonUpload: 'رفع'
+  },
+  copyCode: {
+    text: 'نسخ',
+    successTips: 'تم النسخ!',
+    failTips: 'فشل النسخ!'
+  },
+  mermaid: {
+    flow: 'تدفق',
+    sequence: 'تسلسل',
+    gantt: 'غانت',
+    class: 'فئة',
+    state: 'حالة',
+    pie: 'مخطط دائري',
+    relationship: 'علاقة',
+    journey: 'رحلة'
+  },
+  katex: {
+    inline: 'مضمن',
+    block: 'كتلة'
+  },
+  footer: {
+    markdownTotal: 'عدد الكلمات',
+    scrollAuto: 'تمرير تلقائي'
+  }
+};
+
+export default AR;
