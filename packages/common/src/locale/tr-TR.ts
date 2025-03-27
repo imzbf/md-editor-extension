82b59564e8cd88b15676e448c135b5137d0ae7bd
halillusion <halillusion@gmail.com>
2025-03-27 19:36:49 +0300

    Create tr-TR.ts

---
+++ packages/common/src/locale/tr-TR.ts
@@ -0,0 +1,95 @@
+import type { StaticTextDefaultValue } from '../../index';
+
+/**
+ * @author halillusion
+ * @email  halillusion@gmail.com
+ * @github https://github.com/halillusion
+ *
+ * Türkçe Çeviri
+ *
+ * Yazar çevirinin doğruluğundan emin değildir.
+ * Eğer bir hata bulursanız, lütfen düzeltin.
+ */
+const TR_TR: StaticTextDefaultValue = {
+  toolbarTips: {
+    bold: 'Kalın',
+    underline: 'Altı Çizili',
+    italic: 'İtalik',
+    strikeThrough: 'Üstü Çizili',
+    title: 'Başlık',
+    sub: 'Alt Simge',
+    sup: 'Üst Simge',
+    quote: 'Alıntı',
+    unorderedList: 'Sırasız Liste',
+    orderedList: 'Sıralı Liste',
+    task: 'Görev Listesi',
+    codeRow: 'Satır İçi Kod',
+    code: 'Blok Kod',
+    link: 'Bağlantı',
+    image: 'Resim',
+    table: 'Tablo',
+    mermaid: 'Mermaid Diyagramı',
+    katex: 'Katex Formülü',
+    revoke: 'Geri Al',
+    next: 'İleri',
+    save: 'Kaydet',
+    prettier: 'Düzenle',
+    pageFullscreen: 'Tarayıcı Tam Ekran',
+    fullscreen: 'Tam Ekran',
+    preview: 'Önizleme',
+    htmlPreview: 'HTML Kodu Önizleme',
+    catalog: 'İçindekiler',
+    github: 'Kaynak Kodu'
+  },
+  titleItem: {
+    h1: 'Birinci Seviye Başlık',
+    h2: 'İkinci Seviye Başlık',
+    h3: 'Üçüncü Seviye Başlık',
+    h4: 'Dördüncü Seviye Başlık',
+    h5: 'Beşinci Seviye Başlık',
+    h6: 'Altıncı Seviye Başlık'
+  },
+  imgTitleItem: {
+    link: 'Bağlantı Ekle',
+    upload: 'Resim Yükle',
+    clip2upload: 'Kes ve Yükle'
+  },
+  linkModalTips: {
+    linkTitle: 'Bağlantı Ekle',
+    imageTitle: 'Resim Ekle',
+    descLabel: 'Bağlantı Açıklaması:',
+    descLabelPlaceHolder: 'Lütfen açıklama girin...',
+    urlLabel: 'Bağlantı Adresi:',
+    urlLabelPlaceHolder: 'Lütfen bağlantı girin...',
+    buttonOK: 'Tamam'
+  },
+  clipModalTips: {
+    title: 'Resmi Kes ve Yükle',
+    buttonUpload: 'Yükle'
+  },
+  copyCode: {
+    text: 'Kodu Kopyala',
+    successTips: 'Kopyalandı!',
+    failTips: 'Kopyalama Başarısız!'
+  },
+  mermaid: {
+    flow: 'Akış Diyagramı',
+    sequence: 'Zaman Çizelgesi',
+    gantt: 'Gantt Şeması',
+    class: 'Sınıf Diyagramı',
+    state: 'Durum Diyagramı',
+    pie: 'Pasta Grafiği',
+    relationship: 'İlişki Diyagramı',
+    journey: 'Yolculuk Haritası'
+  },
+  katex: {
+    inline: 'Satır İçi Formül',
+    block: 'Blok Formül'
+  },
+  footer: {
+    markdownTotal: 'Kelime Sayısı',
+    scrollAuto: 'Senkron Kaydırma'
+  }
+};
+
+export default TR_TR;
