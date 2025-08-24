3827d3087a5baba182a5a15d09d17c19a77ef27f
nima parandavar <nimaparsa299@outlook.com>
2025-08-24 13:59:57 +0330

    persian toolbar translations add

---
+++ packages/common/src/locale/fa-ir.ts
@@ -0,0 +1,93 @@
+import type { StaticTextDefaultValue } from "../../index";
+
+/**
+ * @author nima parandavar
+ * @email  nimaparsa299@outlook.com
+ * @github https://github.com/nima-parandavar
+ *
+ * PERSIAN
+ */
+
+const FA_IR: StaticTextDefaultValue = {
+  toolbarTips: {
+    bold: "پررنگ",
+    underline: "زیرخط‌دار",
+    italic: "مورب",
+    strikeThrough: "خط‌خورده",
+    title: "عنوان",
+    sub: "زیرنویس (Subscript)",
+    sup: "بالانویس (Superscript)",
+    quote: "نقل‌قول",
+    unorderedList: "لیست نامرتب",
+    orderedList: "لیست مرتب",
+    task: "لیست وظایف",
+    codeRow: "کد درون‌خطی",
+    code: "کد بلوکی",
+    link: "پیوند",
+    image: "تصویر",
+    table: "جدول",
+    mermaid: "نمودار Mermaid",
+    katex: "فرمول ریاضی",
+    revoke: "بازگردانی",
+    next: "بازگردانی بعدی",
+    save: "ذخیره",
+    prettier: "زیباسازی کد",
+    pageFullscreen: "تمام‌صفحه در صفحه",
+    fullscreen: "تمام‌صفحه",
+    preview: "پیش‌نمایش",
+    previewOnly: "فقط پیش‌نمایش",
+    htmlPreview: "پیش‌نمایش HTML",
+    catalog: "فهرست",
+    github: "کد منبع",
+  },
+  titleItem: {
+    h1: "عنوان سطح ۱",
+    h2: "عنوان سطح ۲",
+    h3: "عنوان سطح ۳",
+    h4: "عنوان سطح ۴",
+    h5: "عنوان سطح ۵",
+    h6: "عنوان سطح ۶",
+  },
+  imgTitleItem: {
+    link: "افزودن لینک تصویر",
+    upload: "بارگذاری تصویر",
+    clip2upload: "برش و بارگذاری",
+  },
+  linkModalTips: {
+    title: "افزودن پیوند",
+    descLabel: "توضیح:",
+    descLabelPlaceHolder: "توضیحی وارد کنید...",
+    urlLabel: "پیوند:",
+    urlLabelPlaceHolder: "آدرس پیوند را وارد کنید...",
+    buttonOK: "تأیید",
+  },
+  clipModalTips: {
+    title: "برش تصویر",
+    buttonUpload: "بارگذاری",
+  },
+  copyCode: {
+    text: "کپی",
+    successTips: "کپی شد!",
+    failTips: "کپی ناموفق!",
+  },
+  mermaid: {
+    flow: "فلوچارت",
+    sequence: "دیاگرام توالی",
+    gantt: "نمودار گانت",
+    class: "دیاگرام کلاس",
+    state: "دیاگرام حالت",
+    pie: "نمودار دایره‌ای",
+    relationship: "ارتباطات",
+    journey: "سفر",
+  },
+  katex: {
+    inline: "درون‌خطی",
+    block: "بلوک",
+  },
+  footer: {
+    markdownTotal: "تعداد کلمات",
+    scrollAuto: "اسکرول خودکار",
+  },
+};
+
+export default FA_IR;
